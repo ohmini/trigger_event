34f00a7c5cde69038151c1bd1891649c59433936
Ratthasorn Thassana <ratthasorn.thassana@gmail.com>
2020-03-26 19:50:11 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"time"
+	"github.com/lib/pq"
+)
+
+func waitForNotification(l *pq.Listener) {
+	for {
+		select {
+		case n := <-l.Notify:
+			fmt.Println("Received data from channel [", n.Channel, "] :")
+			// Prepare notification payload for pretty print
+			var prettyJSON bytes.Buffer
+			err := json.Indent(&prettyJSON, []byte(n.Extra), "", "\t")
+			if err != nil {
+				fmt.Println("Error processing JSON: ", err)
+				return
+			}
+			fmt.Println(string(prettyJSON.Bytes()))
+			return
+		case <-time.After(90 * time.Second):
+			fmt.Println("Received no events for 90 seconds, checking connection")
+			go func() {
+				l.Ping()
+			}()
+			return
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello, world")
+	var conninfo string = "dbname=true4u user=postgres password=1234"
+
+	_, err := sql.Open("postgres", conninfo)
+	if err != nil {
+		panic(err)
+	}
+
+	reportProblem := func(ev pq.ListenerEventType, err error) {
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
+	listener := pq.NewListener(conninfo, 10*time.Second, time.Minute, reportProblem)
+	err = listener.Listen("events")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Start monitoring PostgreSQL...")
+	for {
+		waitForNotification(listener)
+	}
 }
